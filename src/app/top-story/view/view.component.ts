4a508de593e555cea5d033baeaafa51dbfdf3fce
niudai <1244453736@qq.com>
2019-09-20 21:20:10 +0800

    implemented top story nav bar

--- src/app/top-story/view/view.component.ts
+++ src/app/top-story/view/view.component.ts
@@ -2,12 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { TopStoryService } from '../top-story.service';
 import { ActivatedRoute } from '@angular/router';
 
+export interface NavBtnInfo {
+  routerLink: string;
+  name: string;
+  isActive: boolean;
+}
+
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
 export class TopStoryViewComponent implements OnInit {
+
+  navBtnInfos: NavBtnInfo[] = [
+    { routerLink: null, name: '热榜', isActive: true},
+    { routerLink: 'sport', name: '运动', isActive: false},
+    { routerLink: 'science', name: '科学', isActive: false},
+    { routerLink: 'fashion', name: '时尚', isActive: false},
+    { routerLink: 'film', name: '影视', isActive: false},
+    { routerLink: 'digital', name: '数码', isActive: false}
+  ];
+
+  activatedNavBtn: NavBtnInfo;
 
   topStories: any[];
 
@@ -17,7 +34,13 @@ export class TopStoryViewComponent implements OnInit {
     this.route.data.subscribe((data: { stories: any}) => {
       this.topStories = data.stories;
     });
+    this.activatedNavBtn = this.navBtnInfos[0];
     console.log('Hello');
   }
 
+  navBtnToggle(btn: NavBtnInfo) {
+    this.activatedNavBtn.isActive = false;
+    btn.isActive = true;
+    this.activatedNavBtn = btn;
+  }
 }
